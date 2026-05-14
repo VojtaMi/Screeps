e4910b6976bbbf0ac84b108c690c1dc898e4f867
Vojtech Mikel <vmikel@trask.com>
2026-05-14 17:07:51 +0200

    fix builder

--- src/roles/builder.ts
+++ src/roles/builder.ts
@@ -4,13 +4,41 @@ interface BuilderRole extends Role {
   work(creep: Creep): void;
 }
 
+function collectEnergy(creep: Creep): boolean {
+  const energyTarget = creep.findEnergyRefillTarget();
+  if (energyTarget && "amount" in energyTarget) {
+    creep.pickUpEnergy(energyTarget);
+    return true;
+  }
+
+  if (energyTarget) {
+    creep.withdrawEnergyFrom(energyTarget);
+    return true;
+  }
+
+  return false;
+}
+
 export const builder: BuilderRole = {
   run(creep: Creep): void {
-    if (creep.hasEnergy()) {
-      this.work(creep);
-    } else if (creep.needsEnergy()) {
-      creep.goToSource();
+    if (creep.memory.working && !creep.hasEnergy()) {
+      creep.memory.working = false;
     }
+    if (!creep.memory.working && creep.hasFullEnergy()) {
+      creep.memory.working = true;
+      creep.clearEnergyTarget();
+    }
+
+    if (creep.memory.working) {
+      this.work(creep);
+      return;
+    }
+
+    if (collectEnergy(creep)) {
+      return;
+    }
+
+    creep.goToSource();
   },
 
   work(creep: Creep): void {
