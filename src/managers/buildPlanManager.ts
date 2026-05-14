aa0c709317854ec568b26b2cc2985753dcf51e8a
vojta_mi <tami.vojkel@gmail.com>
2026-05-14 20:50:32 +0200

    update build plan

--- src/managers/buildPlanManager.ts
+++ src/managers/buildPlanManager.ts
@@ -13,7 +13,7 @@ const DEFAULT_BUILD_PLANS: Record<string, DefaultBuildPlan> = {
       { x: 41, y: 19, structureType: STRUCTURE_EXTENSION },
       { x: 34, y: 23, structureType: STRUCTURE_EXTENSION },
       { x: 35, y: 23, structureType: STRUCTURE_EXTENSION },
-      { x: 34, y: 22, structureType: STRUCTURE_EXTENSION },
+      { x: 31, y: 18, structureType: STRUCTURE_EXTENSION },
       { x: 39, y: 23, structureType: STRUCTURE_CONTAINER },
       { x: 29, y: 27, structureType: STRUCTURE_CONTAINER },
       { x: 40, y: 12, structureType: STRUCTURE_CONTAINER },
