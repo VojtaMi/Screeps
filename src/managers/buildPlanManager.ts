29e613e58c51bdfc8400394f10ca496dc9730015
vojta_mi <tami.vojkel@gmail.com>
2026-05-15 20:38:04 +0200

    fix road placement

--- src/managers/buildPlanManager.ts
+++ src/managers/buildPlanManager.ts
@@ -25,7 +25,7 @@ const DEFAULT_BUILD_PLANS: Record<string, DefaultBuildPlan> = {
       { x: 34, y: 24, structureType: STRUCTURE_ROAD },
       { x: 34, y: 23, structureType: STRUCTURE_ROAD },
       { x: 35, y: 23, structureType: STRUCTURE_ROAD },
-      { x: 34, y: 23, structureType: STRUCTURE_ROAD },
+      { x: 34, y: 25, structureType: STRUCTURE_ROAD },
       { x: 33, y: 26, structureType: STRUCTURE_ROAD },
       { x: 32, y: 27, structureType: STRUCTURE_ROAD },
       { x: 31, y: 27, structureType: STRUCTURE_ROAD },
